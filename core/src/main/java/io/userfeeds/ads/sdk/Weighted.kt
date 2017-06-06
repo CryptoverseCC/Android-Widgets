1f47c4596bd30c9483be733edf58937ae59ded73
Maciej Górski <maciek.gorski@gmail.com>
2017-06-06 13:51:18 +0200

    [refactoring] Simplify code.

--- core/src/main/java/io/userfeeds/ads/sdk/Weighted.kt
+++ core/src/main/java/io/userfeeds/ads/sdk/Weighted.kt
@@ -3,16 +3,13 @@ package io.userfeeds.ads.sdk
 import java.math.BigDecimal
 import java.util.*
 
-interface Weighted {
+internal interface Weighted {
 
     val weight: BigDecimal
 }
 
 internal fun <T : Weighted> List<T>.randomIndex(random: Random): Int {
-    var sum = BigDecimal.ZERO
-    for (elem in this) {
-        sum += elem.weight
-    }
+    val sum = fold(BigDecimal.ZERO) { acc, elem -> acc + elem.weight }
     var value = BigDecimal(random.nextDouble()) * sum
     forEachIndexed { index, elem ->
         value -= elem.weight
