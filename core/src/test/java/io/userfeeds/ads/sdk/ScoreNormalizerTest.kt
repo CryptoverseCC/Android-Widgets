8ff15696c0aa155afb4b3c338968d54b45ea63cc
Maciej Górski <maciek.gorski@gmail.com>
2017-06-07 09:16:16 +0200

    Should normalize to 67% and 33%.

--- core/src/test/java/io/userfeeds/ads/sdk/ScoreNormalizerTest.kt
+++ core/src/test/java/io/userfeeds/ads/sdk/ScoreNormalizerTest.kt
@@ -3,6 +3,7 @@ package io.userfeeds.ads.sdk
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.math.BigDecimal
+import java.math.RoundingMode.HALF_UP
 
 class ScoreNormalizerTest {
 
@@ -18,13 +19,19 @@ class ScoreNormalizerTest {
         input.assertNormalizedTo("50", "50")
     }
 
+    @Test
+    fun `Should normalize to 67% and 33%`() {
+        val input = listOf("2.0", "1")
+        input.assertNormalizedTo("67", "33")
+    }
+
     private fun List<String>.assertNormalizedTo(vararg expected: String) {
         assertEquals(expected.map(::ad), normalize(this.map(::ad)))
     }
 
     private fun normalize(ads: List<Ad>): List<Ad> {
-        return List(ads.size) { BigDecimal("100") / BigDecimal(ads.size) }
-                .map { Ad("", it, "") }
+        val sum = ads.fold(BigDecimal.ZERO) { acc, ad -> acc + ad.probability }
+        return ads.map { it.copy(probability = (it.probability * BigDecimal("100") / sum).setScale(0, HALF_UP)) }
     }
 }
 
