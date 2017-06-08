0cddf1f151dd94d75a20b05eef8d5a7e27445a5d
Maciej Górski <maciek.gorski@gmail.com>
2017-06-08 15:26:31 +0200

    [refactoring] Rename.

--- core/src/main/java/io/userfeeds/ads/sdk/Normalizer.kt
+++ core/src/main/java/io/userfeeds/ads/sdk/Normalizer.kt
@@ -5,17 +5,17 @@ import java.math.BigDecimal
 import java.math.RoundingMode
 
 internal fun normalize(ads: List<RankingItem>): List<RankingItem> {
-    val sum = ads.fold(BigDecimal.ZERO) { acc, ad -> acc + ad.score }
-    val values = ads.map { it.score * BigDecimal("100") / sum }
-    val roundedDownValues = values.map { it.setScale(0, RoundingMode.DOWN) }
-    val roundedDownSum = roundedDownValues.fold(BigDecimal.ZERO) { acc, value -> acc + value }
-    val someRoundedUp = if (roundedDownSum == BigDecimal("100")) {
-        roundedDownValues
+    val scoreSum = ads.fold(BigDecimal.ZERO) { acc, ad -> acc + ad.score }
+    val probabilities = ads.map { it.score * BigDecimal("100") / scoreSum }
+    val roundedDownProbabilities = probabilities.map { it.setScale(0, RoundingMode.DOWN) }
+    val roundedDownProbabilitiesSum = roundedDownProbabilities.fold(BigDecimal.ZERO) { acc, value -> acc + value }
+    val roundedUpProbabilities = if (roundedDownProbabilitiesSum == BigDecimal("100")) {
+        roundedDownProbabilities
     } else {
-        val reminders = values.zip(roundedDownValues) { value, roundedDown -> value - roundedDown }
-        var numberToRoundUp = (BigDecimal("100") - roundedDownSum).intValueExact()
+        val reminders = probabilities.zip(roundedDownProbabilities) { value, roundedDown -> value - roundedDown }
+        var numberToRoundUp = (BigDecimal("100") - roundedDownProbabilitiesSum).intValueExact()
         val minReminderToRoundUp = reminders.sortedDescending()[numberToRoundUp - 1]
-        roundedDownValues.zip(reminders) { value, reminder ->
+        roundedDownProbabilities.zip(reminders) { value, reminder ->
             value + if (numberToRoundUp > 0 && reminder >= minReminderToRoundUp) {
                 numberToRoundUp--
                 BigDecimal.ONE
@@ -24,5 +24,5 @@ internal fun normalize(ads: List<RankingItem>): List<RankingItem> {
             }
         }
     }
-    return ads.zip(someRoundedUp) { ad, probability -> ad.copy(score = probability) }
+    return ads.zip(roundedUpProbabilities) { ad, probability -> ad.copy(score = probability) }
 }
